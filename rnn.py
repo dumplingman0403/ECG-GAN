b4387689beff624e54e6df15e80626a568071488
Chun Yu "Eric" Wu <ericchunyuwu@gmail.com>
2020-10-13 23:11:51 -0400

    sort code

--- rnn.py
+++ rnn.py
@@ -5,28 +5,29 @@ import pickle
 import numpy as np
 
 
-
 def RNN_model(input_shape):
-    
+
     model = tf.keras.Sequential()
-    model.add(layers.LSTM(20))    #20-100 fine tuning
+    model.add(layers.LSTM(20))  # 20-100 fine tuning
     model.add(layers.Dense(10))
     model.add(layers.Dense(4))
     model.add(layers.Dense(4, activation='softmax'))
 
     opt = tf.keras.optimizers.Adam(lr=0.001)
-    model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy', 'mse'])
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=opt, metrics=['accuracy', 'mse'])
     return model
+
 
 def PrepareInput(train_path, val_path, train_label, val_label):
 
-    train_data  = pickle.load(open(train_path, 'rb'))  
-    val_data    = pickle.load(open(val_path, 'rb'))
+    train_data = pickle.load(open(train_path, 'rb'))
+    val_data = pickle.load(open(val_path, 'rb'))
     train_label = pickle.load(open(train_label, 'rb'))
-    val_label   = pickle.load(open(val_label, 'rb'))
+    val_label = pickle.load(open(val_label, 'rb'))
 
-    x_train     = np.reshape(train_data, (-1, 180, 1))
-    x_val       = np.reshape(val_data, (-1, 180, 1))
+    x_train = np.reshape(train_data, (-1, 180, 1))
+    x_val = np.reshape(val_data, (-1, 180, 1))
 
     y_train = np.array(train_label)
     y_train[y_train == 'N'] = 0
@@ -40,22 +41,25 @@ def PrepareInput(train_path, val_path, train_label, val_label):
     y_val[y_val == 'O'] = 2
     y_val[y_val == '~'] = 3
 
-    y_train     = tf.keras.utils.to_categorical(y_train, 4)
-    y_val       = tf.keras.utils.to_categorical(y_val, 4)
+    y_train = tf.keras.utils.to_categorical(y_train, 4)
+    y_val = tf.keras.utils.to_categorical(y_val, 4)
 
     return [x_train, x_val, y_train, y_val]
+
 
 def Run(epochs):
     PATH_TRAIN = 'data/MedianWave_train.pk1'
     PATH_VAL = 'data/MedianWave_val.pk1'
     TRAIN_LABEL = 'data/label_train.pk1'
     VAL_LABEL = 'data/label_sample.pk1'
-    x_train, x_val, y_train, y_val = PrepareInput(PATH_TRAIN, PATH_VAL, TRAIN_LABEL, VAL_LABEL)
+    x_train, x_val, y_train, y_val = PrepareInput(
+        PATH_TRAIN, PATH_VAL, TRAIN_LABEL, VAL_LABEL)
     Rnn = RNN_model(x_train.shape[1:])
-    history = Rnn.fit(x_train, y_train, epochs=epochs, verbose=1, batch_size=32, validation_data=(x_val, y_val))
+    history = Rnn.fit(x_train, y_train, epochs=epochs, verbose=1,
+                      batch_size=32, validation_data=(x_val, y_val))
     return history
-    
+
+
 if __name__ == "__main__":
 
     history = Run(50)
-    
--- utils.py
+++ utils.py
@@ -3,15 +3,17 @@ import pickle
 from scipy.io import loadmat
 import numpy as np
 import pandas as pd
-from tqdm import  tqdm
+from tqdm import tqdm
+
+
 def load_data(path):
-    name_list = pd.read_csv(os.path.join(path, 'REFERENCE.CSV'), header=None) 
+    name_list = pd.read_csv(os.path.join(path, 'REFERENCE.CSV'), header=None)
     file_list = os.listdir(path)
     file_list = sorted(file_list)
     ECG_records = []
     ECG_name = np.array(name_list.iloc[:, 0])
     ECG_type = np.array(name_list.iloc[:, 1])
-    for i, f in tqdm(enumerate(ECG_name)):  
+    for i, f in tqdm(enumerate(ECG_name)):
         filename = f + '.mat'
         file_path = os.path.join(path, filename)
         ecg = loadmat(file_path)
@@ -19,41 +21,36 @@ def load_data(path):
         ecg_type = ECG_type[i]
 
         ECG_records.append([f, ecg, ecg_type])
-        
-            
+
     return ECG_records
+
 
 def save_as_pickle(var, save_dir):
     save_dir = str(save_dir)
     with open(save_dir, 'wb') as f:
         pickle.dump(var, f)
 
+
 def read_pickle(path):
     data = pickle.load(open(path, 'rb'))
     return data
 
-def normalize(array):
+
+def normalize(array):  # normalize range [-1, 1]
     array = np.array(array)
     max_val = np.max(array)
     min_val = np.min(array)
-    norm_array = (array - min_val)/(max_val - min_val) 
+    norm_array = 2*((array - min_val)/(max_val - min_val)) - 1
     return np.around(norm_array, decimals=3)
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 
-#     PATH_train = "/Users/ericwu/Documents/dataset/af-classification-from-a-short-single-lead-ecg-recording-the-physionet-computing-in-cardiology-challenge-2017-1.0.0/training2017"
-#     PATH_val = "/Users/ericwu/Documents/dataset/af-classification-from-a-short-single-lead-ecg-recording-the-physionet-computing-in-cardiology-challenge-2017-1.0.0/sample2017/validation"
+    PATH_train = "/Users/ericwu/Documents/dataset/af-classification-from-a-short-single-lead-ecg-recording-the-physionet-computing-in-cardiology-challenge-2017-1.0.0/training2017"
+    PATH_val = "/Users/ericwu/Documents/dataset/af-classification-from-a-short-single-lead-ecg-recording-the-physionet-computing-in-cardiology-challenge-2017-1.0.0/sample2017/validation"
 
+    data_train = load_data(PATH_train)
+    data_val = load_data(PATH_val)
 
-#     data_train = load_data(PATH_train)
-#     data_val = load_data(PATH_val)
-
-#     save_as_pickle(data_train, "data_train.pk1")
-#     save_as_pickle(data_val, "data_val.pk1")
-
-    
-
-
-
-
+    save_as_pickle(data_train, "data_train.pk1")
+    save_as_pickle(data_val, "data_val.pk1")
